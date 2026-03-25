b420a934a5477b245f715f9963deb0b65bece73c
keshavsoft <nknnkumar@yahoo.com>
2026-03-25 14:33:55 +0530

    v 1.4.1.1

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 import express from 'express';
 import http from 'http';
-import { router as routerFromV1 } from "./V1/routes.js";
-import { router as routerFromV2 } from "./V2/routes.js";
-import { router as routerFromV3 } from "./V3/routes.js";
 import { router as routerFromV4 } from "./V4/routes.js";
 
 const app = express();
@@ -11,9 +8,6 @@ const server = http.createServer(app);
 var port = normalizePort(process.env.PORT || 3000);
 
 app.use(express.static('Public'));
-app.use("/V1", routerFromV1);
-app.use("/V2", routerFromV2);
-app.use("/V3", routerFromV3);
 app.use("/V4", routerFromV4);
 
 function normalizePort(val) {
