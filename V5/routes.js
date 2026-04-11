23065a19155bd9b9a9329a921a5f312fe983b32b
keshavsoft <nknnkumar@yahoo.com>
2026-04-11 09:50:25 +0530

    V5/BillsTable/InsertToTally perfect

---
+++ V5/routes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { router as routerFromBillsTable } from "./BillsTable/routes.js";
+
+const router = express.Router();
+router.use('/BillsTable', routerFromBillsTable);
+
+export { router };
