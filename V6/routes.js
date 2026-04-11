75bda5afc09daa3687b3a316897994fc60f15d1d
keshavsoft <nknnkumar@yahoo.com>
2026-04-11 10:29:10 +0530

    V6 insert 2 rows

--- V6/routes.js
+++ V6/routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { router as routerFromBillsTable } from "./BillsTable/routes.js";
+import { router as routerFromItemsTable } from "./ItemsTable/routes.js";
 
 const router = express.Router();
 router.use('/BillsTable', routerFromBillsTable);
+router.use('/ItemsTable', routerFromItemsTable);
 
 export { router };
