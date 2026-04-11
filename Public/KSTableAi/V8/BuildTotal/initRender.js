b5f01538157ee63bf5965004ceb6b39cd6da341e
keshavsoft <nknnkumar@yahoo.com>
2026-04-11 17:36:04 +0530

    Delete refresh perfect

---
+++ Public/KSTableAi/V8/BuildTotal/initRender.js
@@ -0,0 +1,57 @@
+import { renderTableHeading } from "./BuildFirstColumn/TableHeading/start.js";
+import { initTable } from "./BuildTableVersions/V1/init.js";
+import { buildForm } from "./BuildForm/start.js";
+import { buildDataLists } from "./BuildDataLists/addToDom.js";
+import { hookAllListeners } from "./AddListeners/start.js";
+
+const initRender = ({ inContainerEl, inDataStore, inDom, inServices, inOptions, inEndPoints, inColumnsConfig }) => {
+    const data = inDataStore.getData();
+    const columns = inDataStore.getColumns();
+    const visibleColumns = inDataStore.getVisibleColumns();
+
+    if (inOptions.dataList.show) {
+        buildDataLists({
+            inContainerEl,
+            inDataStore,
+            inDom
+        });
+    };
+
+    if (inOptions.vertical.showVertical) {
+        buildForm({
+            inContainerEl,
+            inDataStore,
+            inDom
+        });
+    };
+    // debugger;
+    if (inOptions.firstRow.showSearch) {
+        renderTableHeading({
+            inContainerEl,
+            inDataStore,
+            inDom
+        });
+    } else {
+        const firstRow = inDom.getFirstRowClass(inContainerEl);
+
+        firstRow.style.display = "none";
+    };
+
+    // debugger;
+    initTable({
+        inContainerEl,
+        inDataStore,
+        inDom,
+        inServices,
+        inOptions,
+        inEndPoints,
+        inColumnsConfig,
+        inShowFooter: inOptions.table.showFooter,
+        inData: data,
+        inVisibleColumns: visibleColumns
+    });
+
+    hookAllListeners({ inContainerEl, inDataStore, inDom, inOptions });
+};
+
+export { initRender };
--- Public/index.js
+++ Public/index.js
@@ -1,4 +1,4 @@
-import { initTable } from "./KSTableAi/V7/entry.js";
+import { initTable } from "./KSTableAi/V8/entry.js";
 
 const config1 = {
     containerId: 'kSTableContainer',
@@ -70,12 +70,17 @@ const config = {
         { columnName: "ItemName", isRequired: true, tableFooterDataListShow: true },
         { columnName: "Rate", isRequired: false },
         { columnName: "Qty", isRequired: false },
+        { columnName: "Amount", isRequired: false },
+        { columnName: "TaxPer", isRequired: false },
+        { columnName: "Uom", isRequired: false },
+        { columnName: "ParentPk", isRequired: false },
         { columnName: "pk", isPrimaryKey: true, isVisible: false }
     ],
     layout: {
         verticalPosition: "top" // or left/right/bottom
     }
 };
+
 
 let startFunc = async () => {
     await initTable(config);
