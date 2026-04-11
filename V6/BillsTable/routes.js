9521c58d90eef60598dfb9f6a6bb27f4cd4dc38d
keshavsoft <nknnkumar@yahoo.com>
2026-04-11 10:48:15 +0530

    V6 perfect

--- V6/BillsTable/routes.js
+++ V6/BillsTable/routes.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import { router as routerFromInsertToTally } from "./InsertToTally/endpoints.js";
+import { router as routerFromInsert } from "./Insert/endpoints.js";
+import { router as routerFromShowAll } from "./ShowAll/endpoints.js";
 
 const router = express.Router();
 router.use('/InsertToTally', routerFromInsertToTally);
+router.use('/Insert', routerFromInsert);
+router.use('/ShowAll', routerFromShowAll);
 
 export { router };
