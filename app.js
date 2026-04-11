c75384ca477eea0ad4e31989591fc38ab1d7fe6d
keshavsoft <nknnkumar@yahoo.com>
2026-04-11 10:14:29 +0530

    v6 date insert

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import { router as routerFromV4 } from "./V4/routes.js";
 import { router as routerFromV5 } from "./V5/routes.js";
+import { router as routerFromV6 } from "./V6/routes.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -11,6 +12,7 @@ var port = normalizePort(process.env.PORT || 3000);
 app.use(express.static('Public'));
 app.use("/V4", routerFromV4);
 app.use('/V5', routerFromV5);
+app.use('/V6', routerFromV6);
 
 function normalizePort(val) {
     var port = parseInt(val, 10);
