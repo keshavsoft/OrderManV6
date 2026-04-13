9215e6fd9c4110cb5f48ed524cfd794549c74866
keshavsoft <nknnkumar@yahoo.com>
2026-04-13 20:23:05 +0530

    Find perfect

--- Api/V9/BillsTable/end-points.js
+++ Api/V9/BillsTable/end-points.js
@@ -2,8 +2,10 @@ import express from 'express';
 import { getFunc } from "./ShowAll/controller.js";
 import { postFunc } from "./Insert/controller.js";
 import { deleteFunc } from "./Delete/controller.js";
+import { findFunc } from "./Find/controller.js";
 
 const router = express.Router();
+router.get('/Find/:pk', findFunc);
 router.delete('/Delete/:pk', deleteFunc);
 router.post('/Insert', express.json(), postFunc);
 router.get('/ShowAll', getFunc);
