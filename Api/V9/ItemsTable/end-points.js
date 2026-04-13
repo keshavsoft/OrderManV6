035fc32ae568444c52e3245ce81eae2592f8107f
keshavsoft <nknnkumar@yahoo.com>
2026-04-13 21:58:36 +0530

    filter api

--- Api/V9/ItemsTable/end-points.js
+++ Api/V9/ItemsTable/end-points.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { getFunc } from "./ShowAll/controller.js";
+import { filterFunc } from "./Filter/controller.js";
 
 const router = express.Router();
+router.get('/Filter', filterFunc);
 router.get('/ShowAll', getFunc);
 
 export { router };
