dc66cd0976aeda8db378aea2e73630d5dfb6414d
keshavsoft <nknnkumar@yahoo.com>
2026-04-14 22:56:36 +0530

    insert to tally perfect

--- Public/BillsTable/WithSubTable/V5/Index/FormLoad/DomContentLoaded/runAfterDomLoad.js
+++ Public/BillsTable/WithSubTable/V5/Index/FormLoad/DomContentLoaded/runAfterDomLoad.js
@@ -1,4 +1,7 @@
+import { hookAllListeners } from "./AddListeners/start.js";
+
 const runAfterDomLoad = () => {
+    hookAllListeners();
     console.log("DOM Loaded → business logic runs here");
 };
 
